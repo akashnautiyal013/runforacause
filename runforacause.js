98b6688ec397520aeea38200aed33abd855a416a
akashnautiyal013 <akash.nautiyal013@gmail.com>
2016-04-07 22:22:04 +0530

    firstcommit
    run run run

---
+++ runforacause.js
@@ -0,0 +1,167 @@
+const React = require('react-native');
+const SideMenu = require('react-native-side-menu');
+const Menu = require('./Menu.js');
+
+const {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Component,
+} = React;
+
+const styles = StyleSheet.create({
+   title: {
+    fontWeight: '500',
+  },
+  button: {
+    position: 'absolute',
+    top: 20,
+    padding: 10,
+  },
+  caption: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+class Button extends Component {
+  handlePress(e) {
+    if (this.props.onPress) {
+      this.props.onPress(e);
+    }
+  }
+
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={this.handlePress.bind(this)}
+        style={this.props.style}>
+        <Text>{this.props.children}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+module.exports = class runforacause extends Component {
+  state = {
+    isOpen: false,
+    selectedItem: 'About',
+  };
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  updateMenuState(isOpen) {
+    this.setState({ isOpen, });
+  }
+
+  onMenuItemSelected = (item) => {
+    this.setState({
+      isOpen: false,
+      selectedItem: item,
+    });
+  }
+
+  render() {
+    const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
+
+    return (
+      <SideMenu
+        menu={menu}
+        isOpen={this.state.isOpen}
+        onChange={(isOpen) => this.updateMenuState(isOpen)}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+           
+          <Text style={styles.instructions}>
+            To get started, edit index.ios.js
+          </Text>
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+Control+Z for dev menu
+          </Text>
+          <Text style={styles.instructions}>
+            Current selected menu item is: {this.state.selectedItem}
+          </Text>
+        </View>
+        <Button style={styles.button} onPress={() => this.toggle()}>
+          <Image
+            source={{ uri: 'http://i.imgur.com/vKRaKDX.png', width: 32, height: 32, }} />
+        </Button>
+
+      </SideMenu>
+    );
+  }
+};
+
+
+var GeolocationExample = React.createClass({
+  watchID: (null: ?number),
+
+  getInitialState: function() {
+    return {
+      initialPosition: 'unknown',
+      lastPosition: 'unknown',
+    };
+  },
+
+  componentDidMount: function() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = JSON.stringify(position);
+        this.setState({initialPosition});
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = JSON.stringify(position);
+      this.setState({lastPosition});
+    });
+  },
+
+  componentWillUnmount: function() {
+    navigator.geolocation.clearWatch(this.watchID);
+  },
+
+  render: function() {
+    return (
+      <View>
+       <Text>
+          <Text style={styles.title}>Initial position: </Text>
+          {this.state.initialPosition}
+        </Text>
+        <Text>
+          <Text style={styles.title}>Current position: </Text>
+          {this.state.lastPosition}
+        </Text>
+      </View>
+    );
+  }
+});
+
+
